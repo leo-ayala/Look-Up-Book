e07c6c55b70a6b2742c0a4c86eacf1f9541c1fe2
Leo Ayala <leo_a1016@yahoo.com>
2020-11-04 13:58:13 -0500

    added schemas

--- server/server.js
+++ server/server.js
@@ -3,8 +3,16 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require("apollo-server-express");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
